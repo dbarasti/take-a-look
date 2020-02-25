5c0d7dfc27e840fe4ec821fd1944634424deba97
Davide Barasti <dbarasti@gmail.com>
2020-02-25 14:22:08 +0100

    update env variable for rabbitmq url

--- update_worker.py
+++ update_worker.py
@@ -7,12 +7,15 @@ from dotenv import load_dotenv
 
 from scraping_info import ScrapingInfo
 
-app = Celery('scraper', broker='amqp://localhost')
 load_dotenv()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 token = os.getenv("API_TOKEN")
 channel = os.getenv("CHANNEL_NAME")
+
+app = Celery('scraper')
+app.conf.update(BROKER_URL=os.getenv("RABBITMQ_URL"))
+
 bot = telegram.Bot(token=token)
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -20,8 +23,5 @@ logging.basicConfig(level=logging.INFO,
 
 @app.task
 def matching_found(chat_id, web_page_url):
-    duration = 0.5  # seconds
-    freq = 440  # Hz
-    os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
     bot.send_message(chat_id=chat_id,
                      text=f"Found matching at {web_page_url}")
